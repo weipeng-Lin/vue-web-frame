8e6b37ae1c9f67016489c19aa0bcd5a1823301f0
linweipeng <505332716@qq.com>
2023-10-17 15:28:13 +0800

    尝试更新

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler 1
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
